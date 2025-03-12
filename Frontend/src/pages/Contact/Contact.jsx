4cec90fc9630d0231a4615942d30e0a8fdb2c825
Jo <jo-dvplr@gmail.com>
2025-03-12 23:26:18 +0100

    Ajout d'un formulaire de contact avec gestion des soumissions et affichage des résultats. Création d'un fichier SCSS pour le style du composant Contact. A terminer

--- Frontend/src/pages/Contact/Contact.jsx
+++ Frontend/src/pages/Contact/Contact.jsx
@@ -1,7 +1,55 @@
+import { useState } from "react";
+import styles from './Contact.module.scss'
+import SectionTitle from "../../components/UI/SectionTitle/SectionTitle";
+
 function Contact() {
+    const [result, setResult] = useState("");
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        setResult("Sending....");
+        const formData = new FormData(event.target);
+
+        formData.append("access_key", "8720cd99-0cd0-464f-9be5-a47812466e4f");
+
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData
+        });
+
+        const data = await response.json();
+
+        if (data.success) {
+            setResult("Form Submitted Successfully");
+            event.target.reset();
+        } else {
+            console.log("Error", data);
+            setResult(data.message);
+        }
+    };
+
     return (
-        <div>Contact</div>
-    )
+        <section className={styles.contact}>
+            <SectionTitle className={styles.contact__title}>
+                Contact
+            </SectionTitle>
+            <form onSubmit={onSubmit} className={styles.contact__form}>
+                <label htmlFor="name">Name</label>
+                <input type="text" name="name" required />
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email" required />
+                <label htmlFor="message">Message</label>
+                <textarea name="message" required></textarea>
+                <input type="hidden" name="access_key" value="8720cd99-0cd0-464f-9be5-a47812466e4f"></input>
+
+                <button type="submit" className={styles.contact__button}>Envoyer le message</button>
+
+            </form>
+            <span>{result}</span>
+
+        </section>
+    );
 }
+
 
 export default Contact

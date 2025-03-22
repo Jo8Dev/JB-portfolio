8161b56d00a0dca393c4764f1f96935bb2ce83db
Jo <jo-dvplr@gmail.com>
2025-03-22 18:47:29 +0100

    Refactor les styles et les composants Hero pour améliorer la réactivité et la lisibilité, ajout de la gestion des propriétés dans BackgroundHeroCard

--- Frontend/src/components/Sections/Hero/Hero.jsx
+++ Frontend/src/components/Sections/Hero/Hero.jsx
@@ -3,10 +3,15 @@ import styles from "./Hero.module.scss"
 import BackgroundHeroCard from "../../UI/BackgroundHeroCard/BackgroundHeroCard"
 
 function Hero() {
+
+    const cardTitle = "Hello! Moi c'est Joris"
+    const cardSubTitle = "Développeur Web"
+    const cardDescription = "Passionné par la création de solutions web, je suis à la recherche de nouveaux défis au sein d'une équipe dynamique. Mon objectif est de contribuer au succès de projets ambitieux en utilisant des technologies modernes adaptées aux besoins spécifiques de l'entreprise. Ensemble, transformons vos idées en applications efficaces et innovantes."
+
     return (
         <div className={styles.hero}>
-            <BackgroundHeroCard />
-            <HeroCard title={"Hello! Moi c'est Joris"} subTitle={"Développeur Web"} description={"Passionné par la création de solutions web, je suis à la recherche de nouveaux défis au sein d'une équipe dynamique. Mon objectif est de contribuer au succès de projets ambitieux en utilisant des technologies modernes adaptées aux besoins spécifiques de l'entreprise. Ensemble, transformons vos idées en applications efficaces et innovantes."} />
+            <BackgroundHeroCard title={cardTitle} subTitle={cardSubTitle} description={cardDescription} />
+            <HeroCard title={cardTitle} subTitle={cardSubTitle} description={cardDescription} />
         </div>
     )
 }
--- Frontend/src/components/UI/BackgroundHeroCard/BackgroundHeroCard.jsx
+++ Frontend/src/components/UI/BackgroundHeroCard/BackgroundHeroCard.jsx
@@ -1,9 +1,21 @@
 import styles from './BackgroundHeroCard.module.scss'
+import PropTypes from 'prop-types';
 
-function BackgroundHeroCard() {
+
+function BackgroundHeroCard({ title, subTitle, description }) {
     return (
-        <div className={styles.backgroundHeroCard}></div>
+        <div className={styles.backgroundHeroCard}>
+            <h1 className={styles.backgroundHeroCard__title}>{title}</h1>
+            <h2 className={styles.backgroundHeroCard__subtitle}>{subTitle}</h2>
+            <p className={styles.backgroundHeroCard__description}>{description}</p>
+        </div>
     )
+}
+
+BackgroundHeroCard.propTypes = {
+    title: PropTypes.string.isRequired,
+    subTitle: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired
 }
 
 export default BackgroundHeroCard
